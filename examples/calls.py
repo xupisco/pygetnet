1b7e949cc5d35e80be09a2a98ba3d0c1c2b86110
Leandro Voltolino <xupisco@gmail.com>
2020-04-30 14:03:23 -0300

    Trying it out on pycharm...

--- examples/calls.py
+++ examples/calls.py
@@ -10,19 +10,19 @@ def load_fixture(name, **kwargs):
     with open(os.path.dirname(__file__) + '/../getnet/fixtures/' + name + '.json', 'r') as f:
         content = f.read()
     f.close()
-    
+
     for k, v in kwargs.items():
         content = content.replace('_var_' + k, v)
-        
+
     return json.loads(content)
+
 
 api = API(seller_id=os.environ.get('seller_id'),
           client_id=os.environ.get('client_id'),
           client_secret=os.environ.get('client_secret'))
 
-
 new_plan_data = load_fixture('subscription_plan', seller_id=os.environ.get('seller_id'))
-#new_plan = api.post(Plan, data=new_plan_data)
+# new_plan = api.post(Plan, data=new_plan_data)
 
 DOCUMENT_TYPES = ['CPF', 'CNPJ']
 birth_date = datetime.strptime('18/02/1980', '%d/%m/%Y')
@@ -33,7 +33,7 @@ dude_data = load_fixture('customer',
                          customer_id=customer_id,
                          document_type=DOCUMENT_TYPES[0],
                          document_number='123456123456')
-                         
+
 dude, created = api.get_or_create(Customer,
                                   path=[customer_id],
                                   defaults=dude_data)
@@ -56,11 +56,11 @@ if not subs_list.error and subs_list.total > 1:
 # 19/10/2021
 # 182
 
-#sub = api.get(Subscription, path=['8beb3d07-9169-4e85-b45e-0effa5ed653b'])
-#dudes = api.get(Customer, data={'limit': 3})
-#plans = api.get(Plan)
-#plan = api.get(Plan, path=['77aea997-eb43-4f9b-ba8f-5424ea728b17'])
-#cards = api.get(Card, data={'customer_id': 'bd06852a-b560-4f53-8808-659f90d8de3e'})
-#card = api.get(Card, path=['a7aa651a-102c-43a7-ba50-47dc8addddfa'])
+# sub = api.get(Subscription, path=['8beb3d07-9169-4e85-b45e-0effa5ed653b'])
+# dudes = api.get(Customer, data={'limit': 3})
+# plans = api.get(Plan)
+# plan = api.get(Plan, path=['77aea997-eb43-4f9b-ba8f-5424ea728b17'])
+# cards = api.get(Card, data={'customer_id': 'bd06852a-b560-4f53-8808-659f90d8de3e'})
+# card = api.get(Card, path=['a7aa651a-102c-43a7-ba50-47dc8addddfa'])
 
 pass
--- getnet/api.py
+++ getnet/api.py
@@ -32,32 +32,31 @@ class API(BaseResponseHandler):
     environment: int = 0
     access_token: str = None
     access_token_expires: int = None
-                 
+
     def __init__(self,
                  seller_id: str,
                  client_id: str,
                  client_secret: str,
                  environment: int = HOMOLOG):
-        
+
         self.seller_id = seller_id
         self.client_id = client_id
         self.client_secret = client_secret
         self.environment = environment
         self.base_url = API_URLS[environment]
         self.request = requests.Session()
-        
+
         self.request.headers.update({
             "user-agent": "pygetnet-{}".format(__version__),
             "seller_id": self.seller_id,
         })
-        
+
         self._validate_access_token()
-        
+
     def _validate_access_token(self):
         if not self.access_token or \
-            self.access_token_expires < datetime.timestamp(datetime.now()):
-        
-            self.auth()    
+                self.access_token_expires < datetime.timestamp(datetime.now()):
+            self.auth()
 
     def auth(self) -> None:
         path = "/auth/oauth/v2/token"
@@ -65,22 +64,22 @@ class API(BaseResponseHandler):
             "scope": "oob",
             "grant_type": "client_credentials"
         }
-        
+
         response = self.request.post(
             self.base_url + path,
-            data = data,
-            auth = (self.client_id, self.client_secret),
+            data=data,
+            auth=(self.client_id, self.client_secret),
         )
-        
+
         response_data = response.json()
-        
+
         if (response.status_code != 200):
             raise Exception('Auth error {} ({}): {}'.format(
                 response.status_code,
                 response_data.get('error'),
                 response_data.get('error_description'))
             )
-        
+
         self.access_token = response_data.get("access_token")
         self.access_token_expires = int(
             datetime.timestamp(
@@ -90,39 +89,39 @@ class API(BaseResponseHandler):
         self.request.headers.update(
             {"Authorization": "Bearer {}".format(self.access_token)}
         )
-        
+
     def _prepare_request(self, method='get', **kwargs):
         r = kwargs.get('resource', None)
         path = kwargs.get('path', '')
         data = kwargs.get('data', {})
 
         r = r[0] if r else None
-        
+
         if not r and not len(path):
             raise Exception("Resource or path MUST be provided!")
-        
+
         self._validate_access_token()
-        
+
         r = r or Generic
         e = r.get_endpoint() or path[0]
         path = path or [e]
 
         if e not in path:
             path.insert(0, e)
-        
+
         _field = 'qs' if method == 'get' else 'body'
 
-        data = super(API, self).validate_required_params(method, 
-            data, r.get_params().get(method, {}).get(_field, {})
-        )
+        data = super(API, self).validate_required_params(method,
+                                                         data, r.get_params().get(method, {}).get(_field, {})
+                                                         )
 
         return r, e, '/'.join(path), data
-        
+
     def _parse_response(self, resource, endpoint, response):
         data = super(API, self).validate_response(response.json())
         if data.get('error'):
             return GenericResponse(data)
-        
+
         result = {
             'error': False,
             'total': data.get('total') or len(data.get(endpoint, ['dummy'])),
@@ -135,34 +134,34 @@ class API(BaseResponseHandler):
 
         response_data = None
 
-        if data.get('total', 0) or data.get(endpoint):        
-            if (result.get('total') > 1):
-                response_data = { endpoint: [resource(data=fields) for fields in data.get(endpoint)] }
+        if data.get('total', 0) or data.get(endpoint):
+            if result.get('total') > 1:
+                response_data = {endpoint: [resource(data=fields) for fields in data.get(endpoint)]}
             else:
                 response_data = resource(data=data.get(endpoint)[0])
-                
+
         if not response_data:
             response_data = resource(data=data).as_dict()
-        
+
         result.update(response_data)
         return resource(result)
-   
+
     def get(self, *resource, **kwargs):
-        kwargs.update({ 'resource': resource })
+        kwargs.update({'resource': resource})
         r, e, path, data = self._prepare_request(**kwargs)
-        
+
         response = self.request.get(
             self.base_url + '/v1/' + path,
-            params = data
+            params=data
         )
-        
+
         return self._parse_response(r, e, response)
 
     def get_or_create(self, *resource, **kwargs):
         if not kwargs.get('defaults', None):
             raise Exception('You must provide defaults related to the calling Resource with get_or_create.')
 
-        kwargs.update({ 'resource': resource })
+        kwargs.update({'resource': resource})
         r, e, path, data = self._prepare_request(**kwargs)
 
         existing = self.get(r, **kwargs)
@@ -173,7 +172,7 @@ class API(BaseResponseHandler):
         if existing.status_code == 404:
             if not r.can_write():
                 raise Exception('The Resource ({}) is read only!'.format(str(r)))
-                
+
             default_data = {'data': kwargs.get('defaults')}
             data.update(default_data)
 
@@ -185,25 +184,25 @@ class API(BaseResponseHandler):
             return existing, False
 
     def post(self, *resource, **kwargs):
-        kwargs.update({ 'resource': resource })
+        kwargs.update({'resource': resource})
         r, e, path, data = self._prepare_request('post', **kwargs)
-        
+
         response = self.request.post(
             self.base_url + '/v1/' + path,
-            json = data,
-            headers = {"Content-type": "application/json; charset=utf-8"}
+            json=data,
+            headers={"Content-type": "application/json; charset=utf-8"}
         )
-        
+
         return self._parse_response(r, e, response)
-        
+
     def patch(self, *resource, **kwargs):
-        kwargs.update({ 'resource': resource })
+        kwargs.update({'resource': resource})
         r, e, path, data = self._prepare_request('patch', **kwargs)
-        
+
         response = self.request.patch(
             self.base_url + '/v1/' + path,
-            json = data,
-            headers = {"Content-type": "application/json; charset=utf-8"}
+            json=data,
+            headers={"Content-type": "application/json; charset=utf-8"}
         )
-        
+
         return self._parse_response(r, e, response)
